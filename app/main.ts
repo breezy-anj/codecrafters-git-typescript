783d2b403cd300cf607eed699f2de1fc77ce6a53
breezy-anj <windbornanjneya@gmail.com>
2026-07-12 05:50:53 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,19 +1,27 @@
 import * as fs from "fs";
-
+import * as zlib from "zlib";
+import * as path from "path";
 const args = process.argv.slice(2);
 const command = args[0];
 
 switch (command) {
   case "init":
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    console.error("Logs from your program will appear here!");
-
-    // TODO: Uncomment the code below to pass the first stage
     fs.mkdirSync(".git", { recursive: true });
     fs.mkdirSync(".git/objects", { recursive: true });
     fs.mkdirSync(".git/refs", { recursive: true });
     fs.writeFileSync(".git/HEAD", "ref: refs/heads/main\n");
     console.log("Initialized git directory");
+    break;
+
+  case "cat-file":
+    const dir = args[2].substring(0, 2);
+    const fileName = args[2].substring(2);
+    const targetPath = path.join(".git", "objects", dir, fileName);
+    const content = fs.readFileSync(targetPath);
+    const uncompressedContent = zlib.unzipSync(content);
+    const nullByteIndex = uncompressedContent.indexOf(0);
+    const fileContentBuffer = uncompressedContent.subarray(nullByteIndex + 1);
+    process.stdout.write(fileContentBuffer);
     break;
   default:
     throw new Error(`Unknown command ${command}`);
