ec4e6bd79ba6f8aa9bff5a3e2cecbf6c544faada
breezy-anj <windbornanjneya@gmail.com>
2026-07-15 06:34:48 +0530

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -26,13 +26,14 @@ switch (command) {
     break;
   }
   case "hash-object": {
-    const filePath = process.argv.at(-1);
+    const filePath = process.argv.at(-1) as string;
     const fileContent = fs.readFileSync(filePath);
     const length = fileContent.length;
     const headerString = `blob ${length}\0`;
     const buffer = Buffer.from(headerString);
     const payload = Buffer.concat([buffer, fileContent]);
     const hash = crypto.createHash("sha1").update(payload).digest("hex"); //NEW
+
     if (process.argv.includes("-w")) {
       const dir = hash.substring(0, 2);
       const fileName = hash.substring(2);
@@ -42,6 +43,34 @@ switch (command) {
       fs.writeFileSync(path.join(targetPath, fileName), compressed);
     }
     process.stdout.write(hash + "\n");
+    break;
+  }
+
+  case "ls-tree": {
+    const hash = process.argv.at(-1);
+    if (!hash) {
+      console.log("Hash is empty");
+      break;
+    }
+    const dir = hash.substring(0, 2);
+    const fileName = hash.substring(2);
+    const buffer = fs.readFileSync(path.join(".git", "objects", dir, fileName));
+    const uncompressed = zlib.unzipSync(buffer);
+    const nullByteIndex = uncompressed.indexOf(0);
+    let cursor = nullByteIndex + 1;
+
+    const names: string[] = [];
+    while (cursor < uncompressed.length) {
+      const spaceIndex = uncompressed.indexOf(32, cursor); //NEW
+      const nullIndex = uncompressed.indexOf(0, spaceIndex);
+      const nameBuffer = uncompressed.subarray(spaceIndex + 1, nullIndex); // NEW subarray for buffers
+      names.push(nameBuffer.toString("utf-8"));
+
+      cursor = nullIndex + 21;
+    }
+
+    names.sort();
+    names.forEach((name) => process.stdout.write(name + "\n"));
     break;
   }
   default:
